22efa6f4cb43589393e11e6e23652c17eaa96d27
Vanesavsosa <vanesavsosa@gmail.com>
2023-05-24 19:33:22 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,9 @@
+// El area de un rectangulo daria 15
+let base = 5;
+let altura = 3;
+console.log(base * altura)
+
+//El area de un rectangulo daria 66.25
+let base = 12.5;
+let altura = 5.3;
+console.log (base * altura)
---
+++ practico0/promedio3.js
@@ -0,0 +1,20 @@
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+let promedio = (prom1 + prom2 + prom3) / 3;
+console.log (promedio);
+// el promedio es 7
+
+let num1 = -2;
+let num2 = -3;
+let num3 = -8;
+let promedio = (prom1 + prom2 + prom3) / 3;
+console.log (promedio);
+// el promedio es 7
+
+let num1 = -1.2;
+let num2 = 5.3;
+let num3 = -3.3;
+let promedio = (prom1 + prom2 + prom3) / 3;
+console.log (promedio);
+// el promedio es 7
