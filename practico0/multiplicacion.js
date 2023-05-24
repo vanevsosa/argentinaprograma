2023c9fbe0a41c29396739e75961567a09b60ee8
Vanesavsosa <vanesavsosa@gmail.com>
2023-05-24 19:08:36 -0300

    Subir archivo

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,5 @@
+//la multiplicacion daria 204
+let num1 = 2
+let num2 = 17
+let num3 = 6
+console.log (num1 * num2 * num3)
---
+++ practico0/resta.js
@@ -0,0 +1,4 @@
+let num1=10
+let num2=2
+console.log(num1-num2)
+//la restaria daria 8
