7693758920b7929007a6df5dc022c9161bedf429
Vanesavsosa <vanesavsosa@gmail.com>
2023-06-12 20:35:34 -0300

    Subo parte1 de practico1

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,18 @@
+// PARTE 1: VARIABLES 1. Escribir un programa que:
+
+// a. Defina una variable nombre.
+let nombre;
+
+// b. Guarde en la variable nombre tu nombre real.
+nombre = "Vanesa";
+
+// c. Defina y asigne, en un mismo paso, la variable apellido. El valor a asignar deberá ser tu apellido real.
+let apellido = "Sosa";
+
+// d. Repita el paso anterior, pero esta vez definiendo la variable edad y asignandole tu edad real.
+let edad = 41;
+
+// e. Por último, imprime por consola un mensaje que respete el siguiente formato: 
+// "Hola, [nombre] [apellido]. Tienes [edad] años."
+console.log ("Hola, " + nombre + " " + apellido + ". Tienes " + edad + " años.");
+
